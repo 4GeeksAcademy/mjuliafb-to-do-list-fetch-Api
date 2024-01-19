fe90ce3ac71635f8f4622a98bbbf322a6b2c600f
Maria Julia Fernandez Balice (MJ) <136009647+Mjuliafb@users.noreply.github.com>
2024-01-19 20:47:03 +0000

    second commit task add it but with delay

--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
 	const [task, setTask] = useState(null);
 
 	const loadTasks = () => {
-		fetch("https://playground.4geeks.com/apis/fake/todos/user/dsmora")
+		fetch("https://playground.4geeks.com/apis/fake/todos/user/mjulia")
 			.then((resp) => {
 				return resp.json();
 			})
@@ -16,11 +16,25 @@ const Home = () => {
 			})
 	};
 
+	const updateTask = () => {
+		fetch('https://playground.4geeks.com/apis/fake/todos/user/mjulia', {
+			method: "PUT",
+			body: JSON.stringify(task),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(resp => {
+				console.log(resp.ok); // Será true si la respuesta es exitosa
+				console.log(resp.status); // El código de estado 200, 300, 400, etc.
+				console.log(resp.text()); // Intentará devolver el resultado exacto como string
+			})
+	}
+
+
 	useEffect(() => {
 		loadTasks();
 	}, []);
-
-
 
 	const validateInput = (e) => {
 		setInput(e.target.value);
@@ -29,8 +43,16 @@ const Home = () => {
 	const addItem = (e) => {
 		if (e.keyCode === 13) {
 			if (input.trim() !== "") {
-				setList([...list, input]);
+				const inputChange =
+				{
+					id: Math.random(Math.floor() * 999999),
+					label: input,
+					done: false,
+				}
+				setTask([...task, inputChange]);
 				setInput("");
+				updateTask();
+				console.log(task);
 			} else {
 				alert("Por favor, rellena el input antes de añadirlo");
 			}
@@ -49,16 +71,12 @@ const Home = () => {
 			<h1 className="text-center mt-5"> To do list</h1>
 			<input type="text" onChange={validateInput} value={input} onKeyDown={addItem} />
 			<ul className="text-center">
-				{list.map((elemento, index) => (
-					<li key={index}>
-						{elemento} <i className="icon fa-solid fa-x" onClick={() => deleteItem(index)}></i>
-					</li>
-				))}
 				{task && task.map((task, index) => (
 					<li key={index}>{task.label} <i className="icon fa-solid fa-x" onClick={() => deleteItem(index)}> </i></li>
 				))}
 				<li className="itemLeft">{list.length === 0 ? "No hay tareas, añadir tareas" : `${list.length} item${list.length === 1 ? '' : 's'} left`}</li>
 			</ul>
+			<button>Eliminar todas</button>
 		</div>
 	);
 };
